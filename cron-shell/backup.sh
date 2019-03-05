f149503cc3b46200f7a91d87bc15fb8f21a54766
linshenkx <1558867741@qq.com>
2019-03-05 16:22:17 +0800

    修正删除备份文件的正则表达式。

--- cron-shell/backup.sh
+++ cron-shell/backup.sh
@@ -12,4 +12,4 @@ fi
 #将所有数据库导出并按日期命名保存成sql文件并压缩
 /usr/bin/mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"   | gzip > "$DIR/data_`date +%Y%m%d`.sql.gz"
 #查找更改时间在7日以前的sql备份文件并删除
-/usr/bin/find $DIR -mtime +7 -name 'data_[1-9].sql' -exec rm -rf {} \;
+/usr/bin/find $DIR -mtime +7 -name 'data_[1-9]*.sql.gz' -exec rm -rf {} \;
